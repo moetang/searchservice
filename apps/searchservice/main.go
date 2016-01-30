4d9e21f472d6bb2c5d91ef44bbb9e48a21deac92
goodplayer <meidomx@gmail.com>
2016-01-30 22:10:52 +0800

    server with options

--- apps/searchservice/main.go
+++ apps/searchservice/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/moetang/searchservice/server"
 )
 
+var (
+	listenString   string
+	dictionaryPath string
+)
+
+func init() {
+	flag.StringVar(&listenString, "listen", "tcp://0.0.0.0:20100", "e.g. tcp://0.0.0.0:20100")
+	flag.StringVar(&dictionaryPath, "dict", "dictionary.txt", "e.g. /home/user/dictionary.txt")
+
+	flag.Parse()
+}
+
 func main() {
 	config := &server.ServerConfig{
-		DictionaryPath: "dictionary.txt",
-		ListenString:   "tcp://0.0.0.0:15394",
+		DictionaryPath: dictionaryPath,
+		ListenString:   listenString,
 	}
 	server.StartAndListen(config)
 }
